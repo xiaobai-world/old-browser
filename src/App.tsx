01020616b647b4df7638e2fa51b791a87e5daaa7
xiaobai <xiaobai-world@foxmail.com>
2021-06-09 00:16:52 +0800

    optimization struct

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,36 @@
-import React, { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import React, { useState, version } from 'react'
+import Chrome from './icon/Chrome.svg'
+import Firefox from './icon/Firefox.svg'
+import Safari from './icon/Safari.svg'
+import './App.less'
+import { getCurrentBrowserVersion, BrowserMap } from './browser'
+import * as dayjs from 'dayjs'
 
 function App() {
   const [count, setCount] = useState(0)
+  const [browser] = useState(getCurrentBrowserVersion())
+  const currentDate = new Date()
+
+  if (!browser) {
+    return
+  }
+
+  const { version, name } = browser
+
+  const latestVersion = BrowserMap.get(name)?.get(version)
 
   return (
-    <div className="App">
-      
+    <div className="how-old-browser">
+      <div className="browser">
+        <h1>Your use {name || 'Unknow'} <sup>{version}</sup> </h1>
+        {name === 'Chrome' && <img src={Chrome} />}
+        {name === 'Firefox' && <img src={Firefox} />}
+        {name === 'Safari' && <img src={Safari} />}
+      </div>
+      <div className="version">
+        {name} - {version}
+      </div>
+      {latestVersion} - {dayjs(currentDate).format('YYYY-MM-DD')}
     </div>
   )
 }
